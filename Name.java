3c0d735adbf723bfdb1c35447a3df58bdd0a825c
komalarao <116417072+komalarao@users.noreply.github.com>
2022-11-09 05:22:40 -0800

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,15 @@
+package userinput;
+import java.util.*;
+public class Name {
+
+	public static void main(String[] args) {
+		String name;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter your name ");     
+		name= sc.nextLine();
+		System.out.println("entered name is:"+name);
+	}
+	}
+
+
+ 
