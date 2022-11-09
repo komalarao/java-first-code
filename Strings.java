f3dd02e9c4ca576148814d31473e39c7bd9cc31c
komalarao <116417072+komalarao@users.noreply.github.com>
2022-11-09 01:11:31 -0800

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,16 @@
+package comparision;
+
+public class Strings {
+
+	public static void main(String[] args) {
+		 String s1="Capital";
+		   String s2="CApital";
+       if (s1 == s2) {
+    	   System.out.println("true");
+    	   }
+       else {
+    	   System.out.println("false");
+       }
+	}
+
+}
